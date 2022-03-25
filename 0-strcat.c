84f54a245decba5cb2144c993d562680c380233d
Caryourdaysbm <adeyemioluwakayode73@gmail.com>
2022-03-25 15:39:56 -0700

    pointers

--- 0-strcat.c
+++ 0-strcat.c
@@ -1,27 +1,30 @@
 #include "main.h"
 /**
- * _strcat - concatenates two strings
- * @dest: input value
- * @src: input value
- *
- * Return: void
+ *_strcat -  concatenates two strings
+ *@dest: A pointer to an char that will be updated
+ *@src: A pointer to an char that will be updated
+ *Return: dest
  */
+
 char *_strcat(char *dest, char *src)
 {
-int i;
-int j;
-i = 0;
-j = 0;
-while (dest[i] != '\0')
-{
-i++;
-}
-while (src[j] != '\0')
-{
-dest[i] = src[j];
-i++;
-j++;
-}
-dest[i] = '\0';
-return (dest);
+	int i, j;
+
+	i = 0;
+	while (dest[i] != '\0') /*WHILE count character */
+	{
+		i++;
+	}
+
+	j = 0;
+	while (src[j] != '\0') /*WHILE concatenate dest with src*/
+	{
+		dest[i] = src[j];
+		j++;  /*sum j*/
+		i++; /**sum i*/
+	}
+
+	dest[i] = '\0'; /*the terminating null byte*/
+
+	return (dest);
 }
--- 1-strncat.c
+++ 1-strncat.c
@@ -1,30 +1,32 @@
 #include "main.h"
 /**
- * _strncat - concatenate two strings
- * using at most n bytes from src
- * @dest: input value
- * @src: input value
- * @n: input value
- *
- * Return: dest
+ *_strncat -  concatenates two strings
+ *@dest: A pointer to an char that will be updated
+ *@src: A pointer to an char that will be updated
+ *@n: value
+ *Return: dest
  */
-char *_strncat(char *dest, char *src, int n)
-{
-int i;
-int j;
 
-i = 0;
-while (dest[i] != '\0')
+char *_strncat(char *dest, char *src, int n)
+
 {
-i++;
-}
-j = 0;
-while (j < n && src[j] != '\0')
-{
-dest[i] = src[j];
-i++;
-j++;
-}
-dest[i] = '\0';
-return (dest);
+	int i, j;
+
+	i = 0;
+	while (dest[i] != '\0') /*WHILE count character */
+	{
+		i++;
+	}
+
+	j = 0;
+	while (j < n && src[j] != '\0') /*WHILE concatenate dest with src*/
+	{
+		dest[i] = src[j];
+		j++;  /*sum j*/
+		i++; /**sum i*/
+	}
+
+	dest[i] = '\0'; /*the terminating null byte*/
+
+	return (dest);
 }
